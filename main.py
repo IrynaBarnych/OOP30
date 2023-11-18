6b67ea9b325f6c2cc01e7201b5931d832a966bb9
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-18 22:03:50 +0200

    конспект

--- main.py
+++ main.py
@@ -25,17 +25,14 @@ class C:
     def __init__(self):
         print('c')
 
-
 class A:
     def __init__(self):
         print('a')
-
 
 class B(A):
     def __init__(self):
         super().__init__()
         print('b')
-
 
 class D(B, A):
     def __init__(self):
