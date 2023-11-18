d9e7c992959c70df59846a6577a56b89dfe36ca9
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-18 22:11:48 +0200

    додала код

--- main.py
+++ main.py
@@ -6,3 +6,58 @@
 # ■ Sound — видає звук тварини (пишемо текстом в консоль);
 # ■ Show — відображає ім’я тварини;
 # ■ Type — відображає підвид тварини.
+
+class DomesticAnimal:
+    def __init__(self, name, type_of_animal):
+        self.name = name
+        self.type_of_animal = type_of_animal
+
+    def sound(self):
+        pass
+    def show(self):
+        print(f"Ім'я {self.name}")
+    def type(self):
+        pass
+class Dog(DomesticAnimal):
+    def sound(self):
+        print("Woof!")
+
+    def show(self):
+        super().show()
+
+    def type(self):
+        print(f"Тип тварини {self.type_of_animal}")
+
+class Cat(DomesticAnimal):
+    def sound(self):
+        print("Meow!")
+
+    def show(self):
+        super().show()
+
+    def type(self):
+        print(f"Тип тварини {self.type_of_animal}")
+
+class Parrot(DomesticAnimal):
+    def sound(self):
+        print("Ку-ка-рі-ку")
+
+    def show(self):
+        super().show()
+
+    def type(self):
+        print(f"Тип тварини {self.type_of_animal}")
+
+class Hamster(DomesticAnimal):
+    def sound(self):
+        print("звуки гризуна")
+
+    def show(self):
+        super().show()
+
+    def type(self):
+        print(f"Тип тварини {self.type_of_animal}")
+dog = Dog("Rey", "dog")
+dog.sound()
+dog.show()
+dog.type()
