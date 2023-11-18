fe42883754982534f6c7e2afc7334acd9b5195b6
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-18 23:01:59 +0200

    додала код за формулою пі*0,5*а**2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Завдання 1
 # Використовуючи поняття множинного успадкування,
 # створіть клас «Коло, поміщене в квадрат».
+#Варіант 2
 
 import math
 
@@ -20,6 +21,8 @@ class Square:
 
 class CircleInSquare(Circle, Square):
     def __init__(self, radius, side):
+        Circle.__init__(self, radius)
+        Square.__init__(self, side)
         self.circle = Circle(radius)
         self.square = Square(side)
 
@@ -32,5 +35,24 @@ print("Площа круга:", circle_in_square.circle.area())
 print("Площа квадрата:", circle_in_square.square.area())
 print("Залишкова площа:", circle_in_square.remaining_area())
 
+print()
+
 #Варіант 2
+
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+class Square(Circle):
+    def __init__(self, side):
+        super().__init__(side)
+
+square_in_circle = Square(0.5)
+
+print(f"Площа Кола, поміщеного в квадрат: {square_in_circle.area()}")
+
 
